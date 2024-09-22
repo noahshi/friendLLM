641b42659415bd9b4c5d4b56774456fb7957bad5
Noah Shi <noah.shi@outlook.com>
2024-09-21 23:33:47 -0700

    Moved CasualSelfAttention class to gpt.py
    Moved attention back to with the other model classes

--- gpt.py
+++ gpt.py
@@ -12,6 +12,33 @@ class GPTConfig:
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
+    
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+    
+    def forward(self, x):
+        B, T, C = x.size()
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
+        
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
 
 class MLP(nn.Module):
     def __init__(self, config) -> None:
